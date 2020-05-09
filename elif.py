e07e96b205602acbae474e39d566e9fbc0f8324d
supercoolgit <vishnu.bairi999@gmail.com>
2020-05-09 15:26:03 -0700

    adding new new folder

---
+++ elif.py
@@ -0,0 +1,52 @@
+
+# print(list(range(100)))
+
+# for v in range(70,1010):
+#     print(v)
+
+
+# v=1
+# while v<=100:
+#     print(v)
+#     v=v+10
+
+# for i in range(1,10):
+#     if i==5:
+#         break
+#     print(i)
+# print("vishnu")
+
+# for v in range(1,10):
+#     if v==8:
+#         continue
+#     print(v)
+# print("exit from program")
+
+# for v in range(1,10):
+#     if v==8:
+#         break
+#     print(v)
+# print("exit from program")
+
+
+
+print(max(88.44,44,-30))
+print(min(88.44,44,-33))
+print(max(88.44,44,-39))
+print(min(88.44,44,-30))
+print(max(88.44,44,-30))
+print(min(88.44,44,-33))
+print(max(88.44,44,-39))
+print(min(88.44,44,-30))
+print(max(88.44,44,-30))
+print(min(88.44,44,-33))
+print(max(88.44,44,-39))
+print(min(88.44,44,-30))
+print(max(88.44,44,-30))
+print(min(88.44,44,-33))
+print(max(88.44,44,-39))
+print(min(88.44,44,-30))
+
+
+
+
